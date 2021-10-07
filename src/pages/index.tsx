ea5ea493ab223be68a5891cd6b3ca71d265baf68
Diogo Almazan <diogodpa@outlook.com>
2021-10-06 22:49:41 -0300

    Stripe configurado e Static Site Generation para a Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
 import Head from "next/head";
+import { GetStaticProps } from "next";
 import { SubscribeButton } from "../components/SubscribeButton";
 import styles from "./home.module.scss";
+import { stripe } from "../services/stripe";
 
-export default function Home() {
+// Client-side: Quando não precisa de indexação.
+// Server-side: Quando se rpecisa da indexação e também precisa de dados dinâmicos na página (Informações do usuário que está acessando)
+// Static Site Generation: Ter uma pagia estatica compartilhada com todas as pessoas que estão utilizando a aplicação (Post de um blog em si). Precisam de SEO
+
+interface HomeProps {
+	product: {
+		priceId: string;
+		amount: number;
+	};
+}
+
+export default function Home({ product }: HomeProps) {
 	return (
 		<>
 			<Head>
@@ -16,14 +29,38 @@ export default function Home() {
 					</h1>
 
 					<p>
-						Get acces to all the publications <br />
-						<span>for $9.90 month</span>
+						Get access to all the publications <br />
+						<span>for {product.amount} month</span>
 					</p>
 
-					<SubscribeButton />
+					<SubscribeButton priceId={product.priceId} />
 				</section>
 				<img src="/images/avatar.svg" alt="Girl coding" />
 			</main>
 		</>
 	);
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+	const prices = await stripe.prices.retrieve(
+		"price_1JhgOIHwmw9NaWLYl2cLuSof",
+		{
+			expand: ["product"],
+		}
+	);
+
+	const product = {
+		priceId: prices.id,
+		amount: new Intl.NumberFormat("en-US", {
+			style: "currency",
+			currency: "USD",
+		}).format(prices.unit_amount / 100), // centavos
+	};
+
+	return {
+		props: {
+			product,
+		},
+		revalidate: 60 * 60 * 24, // em segundos -> 24 horas
+	};
+};
