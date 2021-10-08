b936ed48604c203337ce6da4edfc997847632594
Diogo Almazan <diogodpa@outlook.com>
2021-10-07 22:47:39 -0300

    Next-auth adicionado e configurações de autenticação com GitHub

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,15 @@
 import { AppProps } from "next/app";
 import { Header } from "../components/Header";
+import { Provider as NextAuthProvider } from "next-auth/client";
 import "../styles/global.scss";
 
 function MyApp({ Component, pageProps }: AppProps) {
+	//As informações do usuário logado, quando dá refresh, vem do pageProps.session
 	return (
-		<>
+		<NextAuthProvider session={pageProps.session}>
 			<Header />
 			<Component {...pageProps} />
-		</>
+		</NextAuthProvider>
 	);
 }
 
