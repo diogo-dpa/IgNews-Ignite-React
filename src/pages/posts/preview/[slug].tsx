298a8dbaadc6f2889209b01ba1efbed4e9dcb9ee
Diogo Almazan <diogodpa@outlook.com>
2021-12-03 23:36:54 -0300

    Ajuste no getStaticPaths

--- src/pages/posts/preview/[slug].tsx
+++ src/pages/posts/preview/[slug].tsx
@@ -1,4 +1,4 @@
-import { GetStaticProps } from "next";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useSession } from "next-auth/client";
 import Head from "next/head";
 import Link from "next/link";
@@ -56,9 +56,15 @@ export default function PostPreview({ post }: PostPreviewProps) {
 	);
 }
 
-export const getStaticPaths = () => {
+export const getStaticPaths: GetStaticPaths = async () => {
 	return {
-		paths: [],
+		paths: [
+			{
+				params: {
+					slug: "",
+				},
+			},
+		],
 		fallback: "blocking",
 	};
 };
@@ -90,5 +96,6 @@ export const getStaticProps: GetStaticProps = async ({ params }) => {
 		props: {
 			post,
 		},
+		revalidate: 60 * 30, // 30 minutos
 	};
 };
